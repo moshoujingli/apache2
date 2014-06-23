a4e70932f9721af796bd184f892edcb421c3d210
Bixiaopeng <bixiaopeng2007@gmail.com>
2014-06-23 16:30:21 +0900

    fix path

--- recipes/default.rb
+++ recipes/default.rb
@@ -192,16 +192,14 @@ service 'apache2' do
     # This may happen with multiple recipes notifying apache to restart - like
     # during the initial bootstrap.
     restart_command '/sbin/service httpd restart && sleep 1'
-    reload_command '/sbin/service httpd reload && sleep 1'
   when 'debian'
     service_name 'apache2'
     restart_command '/usr/sbin/invoke-rc.d apache2 restart && sleep 1'
-    reload_command '/usr/sbin/invoke-rc.d apache2 reload && sleep 1'
   when 'arch'
     service_name 'httpd'
   when 'freebsd'
     service_name 'apache22'
   end
-  supports [:restart, :reload, :status]
+  supports [:restart, :status]
   action [:enable, :start]
 end
